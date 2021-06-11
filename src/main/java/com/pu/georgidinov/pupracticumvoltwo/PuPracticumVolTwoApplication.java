7af0bf603fd0f8d35b32003e8f5f5a094939872a
Georgi Dinov <georgidinov1@abv.bg>
2021-06-11 19:27:50 +0300

    Initial commit

---
+++ src/main/java/com/pu/georgidinov/pupracticumvoltwo/PuPracticumVolTwoApplication.java
@@ -0,0 +1,13 @@
+package com.pu.georgidinov.pupracticumvoltwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuPracticumVolTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PuPracticumVolTwoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pu/georgidinov/pupracticumvoltwo/PuPracticumVolTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pu.georgidinov.pupracticumvoltwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuPracticumVolTwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
